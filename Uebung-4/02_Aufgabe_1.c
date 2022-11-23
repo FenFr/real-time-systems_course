ffa6d5ba23295975b8cd8e43357b7a1c6ff895cf
FenFr <HohesCAlt@gmail.com>
2022-11-23 23:47:31 +0100

    worked on the work

--- Uebung-4/02_Aufgabe_1.c
+++ Uebung-4/02_Aufgabe_1.c
@@ -12,13 +12,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>        // for ftok()
+
+
+struct msgbuf {
+    long mtype;
+    char mtext[100];
+};
 
 
 // MAIN //////////////////////////////////////////////////////////////////////
 
 int main(int argc, char**argv) {
 
+    key_t key = ftok("02_Aufgabe_1.c", 1);
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+
+    struct msgbuf message;
+    message.mtype = 1;
     
+    printf("Write Message: ");
+    gets()
+
+    if( (msgsnd(key,  &message, sizeof(message), 0)) == -1 ) {
+        printf("Error: Could not send message");
+        exit(1);
+    }
 
     return 0;
 }
